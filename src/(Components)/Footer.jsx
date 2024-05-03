6be1f1915f52674db45b2129b610dd97b7ce15ce
Ayaz Shahnawaz Qureshi <131868270+ayazq18@users.noreply.github.com>
2024-05-04 03:56:25 +0530

    added footer

---
+++ src/(Components)/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div style={{textAlign:'center', marginTop:'20px', color:'#808080'}}>
+      &copy; Made by Ayaz Qureshi
+    </div>
+  )
+}
+
+export default Footer
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,16 @@
+import Footer from "@/(Components)/Footer";
 import SalesSection from "@/(Components)/SalesSection";
 import SideNavBar from "@/(Components)/SideNavBar";
 import { Grid } from "@mui/material";
 
 export default function Home() {
   return (
+    <>
     <Grid container spacing={0} gap={5} sx={{}}>
       <SideNavBar />
       <SalesSection />
     </Grid>
-
-
+    <Footer/>
+    </>
   );
 }
